b9e906f913c4ff078a9b5648f4eec87f7beb166b
beva3 <mizabienvenu@gmail.com>
2024-09-30 05:38:05 +0530

    done

--- Occurrences.c
+++ Occurrences.c
@@ -7,9 +7,15 @@
 
 int  main(){
 
-    int myArray[] = {1,4,6,6,7};
+    int myArray[] = {6,4,6,6,7};
     int count = 0;
     int to_Find = 6;
+
+    for(int i=0; i<5; i++){
+
+        if(myArray[i] == to_Find)
+            count++;
+    }
 
     printf("# of %ds found : %d\n",to_Find, count);
 
