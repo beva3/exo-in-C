ec23d25fe87f3f50e45c4f865ca27aa8f763823a
beva3 <mizabienvenu@gmail.com>
2024-09-30 05:34:57 +0530

    Count the occurrences of a value in a array

---
+++ Occurrences.c
@@ -0,0 +1,17 @@
+/**
+ * Count the Occurrences of a value in a Array
+ * mijery izay miverina no tiana atongavana
+ */
+
+#include <stdio.h>
+
+int  main(){
+
+    int myArray[] = {1,4,6,6,7};
+    int count = 0;
+    int to_Find = 6;
+
+    printf("# of %ds found : %d\n",to_Find, count);
+
+    return 0;
+}
