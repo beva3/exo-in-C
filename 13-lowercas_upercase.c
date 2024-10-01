699e49ac2ad1793b0b52a5ad34bcffd04d09a12f
beva3 <mizabienvenu@gmail.com>
2024-10-01 18:54:53 +0530

    run

---
+++ 13-lowercas_upercase.c
@@ -0,0 +1,30 @@
+/**
+ * Flip (retourner) a string's lowercase characters to uppercase and vice versa
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+void letter_flip(char *s){
+    
+}
+
+int main(){
+
+    char s[] = "abcdABCD";
+    int length = strlen(s);
+
+    for (int i = 0; i < length; i++)
+    {
+        if (isupper(s[i])){
+            s[i] = tolower(s[i]);
+        }else if(islower(s[i])){
+            s[i] = toupper(s[i]);
+        }
+    }
+    
+    printf("%s\n",s);
+
+    return 0;
+}
