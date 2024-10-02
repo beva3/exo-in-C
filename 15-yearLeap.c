83c053901e58f4868ce84ab5817576cee4313697
beva3 <mizabienvenu@gmail.com>
2024-10-02 14:44:39 +0530

    y%4 !=0 0, y%100 != 1, y%400 != 0 0

--- 15-yearLeap.c
+++ 15-yearLeap.c
@@ -8,6 +8,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+bool is_leap_years(int year);
+
 int main(){
     // char cmd[] = "cal 2 -m 2024";
     // for (int i = 2016; i <= 2200; i++)
@@ -39,7 +41,20 @@ int main(){
     its creat a new fil : cal, and store the result of cmd, after you can see the result in this file
  * */  
 
-    
-    
+    for (int year = 2016; year <= 3000; year++)
+    {
+        if(!is_leap_years(year)){
+            sprintf(cmd,"cal -m 2 %d >> CAL",year);
+            system(cmd);
+            // sleep(1);
+        }
+    }
     return 0;
+}
+
+bool is_leap_years(int year){
+    if(year%4 != 0) return false;
+    else if (year % 100 != 0) return true;
+    else if (year % 400 != 0) return false;
+    else return true;
 }
