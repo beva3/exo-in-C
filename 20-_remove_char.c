eadccc8e83efdb753d407cbbd1c175d824397115
beva3 <mizabienvenu@gmail.com>
2024-10-04 09:50:53 +0530

    affectation lasa mamafa @ string ??, que veut dire remove ???

--- 20-_remove_char.c
+++ 20-_remove_char.c
@@ -2,6 +2,8 @@
 
 /*-------------------------------------*/
 void remove_char(char *s, char c);
+void print_array(char *array);
+/*-------------------------------------*/
 
 int main(){
     char str[] = "raphael";
@@ -9,10 +11,34 @@ int main(){
     return 0;
 }
 
+
+void print_array(char *array){
+    int i=0;
+    while (array[i] != '\0')
+    {
+        printf("%c%c",array[i],(array[i] == '\0') ? '\n' : ' ' );
+        i++;
+    }
+    printf("[%d]\n",i-1);
+    
+}
+
 void remove_char(char *s, char c){
     int i=0;
     while (s[i])
     {
+        if (s[i] == c)
+        {
+            int position_tmp = i; 
+            while (s[position_tmp] != '\0')
+            {
+                s[position_tmp] = s[position_tmp + 1];
+                position_tmp++;
+                print_array(s);
+            }
+            
+        }
+        
         
         printf("[%c]  => [%s] %c\n",s[i],(s[i] == c) ? "remove":"ok",(s[i + 1] != '\0') ? ' ' : '\n');
         
