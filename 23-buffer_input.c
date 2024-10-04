161236dada22d056e0614c680625e9630aa0aadf
beva3 <mizabienvenu@gmail.com>
2024-10-04 23:15:57 +0530

    buffer, size, allocation, infinit output

--- 23-buffer_input.c
+++ 23-buffer_input.c
@@ -1,23 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define BUFFER_SIZE 5
 
 int main(void){
 
-    int buffer[BUFFER_SIZE]={0};
+    // int buffer[BUFFER_SIZE]={0};
+    // int total = 0;
+
+
+    // for (int i = 0; i < BUFFER_SIZE; i++)
+    // {
+    //     printf("Enter :");
+    //     scanf("%d",&buffer[i]);
+    // }
+    // for (int  i = 0; i < BUFFER_SIZE ; i++)
+    // {
+    //     printf("buffer[%d] = %d\n",i,buffer[i]);
+    //     total += buffer[i];
+    // }
+    // printf("Average = %d\n",total/BUFFER_SIZE);
+
+    int size = 2;
+    int *buffer = malloc(sizeof(int)*size);
+    int input = 0;
+    int num_data = 0;
     int total = 0;
 
-
-    for (int i = 0; i < BUFFER_SIZE; i++)
+    while (1)
     {
-        printf("Enter :");
-        scanf("%d",&buffer[i]);
+
+        if(num_data == size){
+            size *= 2;
+            buffer = realloc(buffer,sizeof(int)*size);
+        }
+
+        printf("Enter : "); scanf("%d",&input);
+        if(input == -1) break;
+        else buffer[num_data] = input;
+        num_data++;
     }
-    for (int  i = 0; i < BUFFER_SIZE ; i++)
+    
+    for (int  i = 0; i < num_data ; i++)
     {
         printf("buffer[%d] = %d\n",i,buffer[i]);
         total += buffer[i];
+        
     }
-    printf("Average = %d\n",total/BUFFER_SIZE);
+    printf("Average = %d\n",total/num_data);
+
 
     return 0;
 }
