1b2655c5ea4227307f6d27542540057ab7b29949
beva3 <mizabienvenu@gmail.com>
2024-10-09 05:05:06 +0530

    isdigit() - Vérifie si le caractère est un chiffre (0-9)

--- ctype/is_type.c
+++ ctype/is_type.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+
+/**
+ * 
+ *     isalpha() - Vérifie si le caractère est une lettre (a-z, A-Z).
+
+    isdigit() - Vérifie si le caractère est un chiffre (0-9).
+
+    isalnum() - Vérifie si le caractère est alphanumérique (lettre ou chiffre).
+
+    islower() - Vérifie si le caractère est une lettre minuscule.
+
+    isupper() - Vérifie si le caractère est une lettre majuscule.
+
+    isspace() - Vérifie si le caractère est un espace blanc (espace, tabulation, nouvelle ligne, etc.).
+
+    tolower() - Convertit une lettre majuscule en minuscule.
+
+    toupper() - Convertit une lettre minuscule en majuscule.
+ */
+
 void is_ALPHA(){
     char alpha[]="qwerQWER_)123~";
     int i=0;
@@ -11,9 +31,20 @@ void is_ALPHA(){
     printf("\n");
 }
 
+void is_DIGIT(){
+    char alpha[]="123!@#as";
+    int i=0;
+    printf("--------------------------------------------------------------\n\n");
+    printf("isdigit() - Vérifie si le caractère est un chiffre (0-9)\n");
+    while (alpha[i])
+        printf("%c | %d\n",alpha[i],isdigit(alpha[i])?1:0),i++;
+    printf("\n");
+}
+
 int main()
 {
     is_ALPHA();
+    is_DIGIT();
 
     return 0;
 }
