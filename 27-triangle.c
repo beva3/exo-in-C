e3a451043a6a8cb1b67910e4c770cf860de809f6
beva3 <mizabienvenu@gmail.com>
2024-10-09 08:57:07 +0530

    edid a a message

--- 27-triangle.c
+++ 27-triangle.c
@@ -20,14 +20,14 @@ void make_triangle(int number_line,const char* mode){
 
     else
         for (int  i = 0; i <= number_line; i++)
-            printf("okaloha\n");
+            printf("try to use the const \"sub\" or \"sup\"\n");
 }
 
 void make_triangle_sup(int number_line){
     for (int  i = number_line; i >= 0; i--)
     {
         print_o(i);
-    }
+    } 
     
 }
 
