d4ea34ec1a6e807c0cdd74aef966cda1a74d1267
beva3 <mizabienvenu@gmail.com>
2024-10-09 06:08:20 +0530

    make sub sup triangle

---
+++ 27-triangle.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+void print_o(int number_o){
+    for(int i=0; i<number_o; i++)
+        printf("o%c",(i==number_o-1) ? '\n':' ');
+}
+
+void make_triangle(int number_line,const char* mode){
+    int start = 0;
+    printf("triangle %s\n",mode);
+
+    if (strcmp(mode,"sup") == 0)
+        for (int  i = number_line; i >= 0; i--)
+            print_o(i);
+
+    else if(strcmp(mode,"sub") == 0)
+        for (int  i = 0; i <= number_line; i++)
+            print_o(i);
+
+    else
+        for (int  i = 0; i <= number_line; i++)
+            printf("okaloha\n");
+}
+
+void make_triangle_sup(int number_line){
+    for (int  i = number_line; i >= 0; i--)
+    {
+        print_o(i);
+    }
+    
+}
+
+int main(){
+    make_triangle(9,"sub");
+    return 0;
+}
