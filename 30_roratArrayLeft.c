a8bc2852c639821387b8ecf9a29602dfa25e4236
beva3 <mizabienvenu@gmail.com>
2024-10-10 22:32:42 +0530

    rotat right

--- 30_roratArrayLeft.c
+++ 30_roratArrayLeft.c
@@ -14,6 +14,10 @@
 
 void rotate_left_once(int array[], int length);
 void rotate_left(int array[], int length,int n);
+
+void rotate_right_once(int array[], int length);
+void rotate_right(int array[], int length,int n);
+
 void print_array(int array[], int length);
 
 int main(){
@@ -36,9 +40,11 @@ int main(){
     // print_array(a1,6);
 
     // rotate_left_once(a1,6);
-    // print_array(a1,6);
     
-    rotate_left(a1,6,2);
+    print_array(a1,6);
+    // rotate_right_once(a1,6);
+    rotate_right(a1,6,3);
+    print_array(a1,6);
 
     return 0;
 }
@@ -63,4 +69,23 @@ void rotate_left(int array[], int length,int n){
 void print_array(int array[], int length){
     for(int i=0; i<length; i++)
         printf("%d%c",array[i],(i == length-1) ? '\n' :' ');
+}
+
+void rotate_right_once(int array[], int length){
+    int tmp = array[length-1];
+    printf("Rotate an array left once...\n");
+    for(int i = length-2; i>=0; i--)
+        array[i+1] = array[i]; // a[2] <= a[1]
+    array[0] = tmp;
+}
+
+void rotate_right(int array[], int length,int n){
+    for(int i=0; i<n; i++)
+        rotate_left_once(array,length);
+
+    /**
+     * 1234
+     *  2341
+     *   3412
+     */
 }
