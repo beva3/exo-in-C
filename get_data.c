38503c360e90fe7f9c933330da57c286b7f6a188
beva3 <mizabienvenu@gmail.com>
2024-10-11 08:35:28 +0530

    get data, use the pointor

---
+++ get_data.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+void get_data(int *n1,int *n2);
+void print_data(int n1, int n2);
+
+/**
+ * void get_data(int *n1,int *n2) :
+ *      void : la focnction ne return aucune valeur
+ *      get_data : nom de la fonction 
+ *      int *n1, int *n2 : parametre de la fonction :
+ *          des poiteur vers des intiers
+ *      
+ *      cela implique, c'est que : det_data peut modifier les caleurs des entiers
+ *      poinetee par n1 et n2 directement, car elle recoit lesur adress memoir
+ * 
+ *      alaina aiza : mitaky adress 
+ */
+
+int main(){
+
+    int n1 = 0, n2 = 0;
+
+    get_data(&n1,&n2);
+    print_data(n1,n2);
+
+
+    return 0;
+}
+
+void print_data(int n1, int n2){
+    printf("Print data ... :\n");
+    printf("n1 = %d\n",n1);
+    printf("n2 = %d\n",n2);
+}
+
+void get_data(int *n1,int *n2){
+    int a = 20,
+        b = 39;
+
+    printf("Get data ...\n");
+
+    *n1 = a;
+    *n2 = b;
+}
