37f48107a326b4073a4d3c742c1a1387cfda7cf9
beva3 <mizabienvenu@gmail.com>
2024-10-12 14:56:31 +0530

    creat function sort bubble

--- bubble_short.c
+++ bubble_short.c
@@ -5,6 +5,7 @@
  * tri a bule
  */
 void sawp(int *a,int *b);
+void bubble_sort(int a[], int length);
 void print_array(int array[], int length);
 
 int main(){
@@ -14,12 +15,15 @@ int main(){
 
     print_array(a,length);
 
-    for(int i=0; i<length; i++){
-        for(int j=0; j<(length-1); j++){
-            if(a[j]>a[j+1])
-                sawp(&a[j], &a[j+1]);
-        }
-    }
+    // for(int i=0; i<length; i++){
+    //     for(int j=0; j<(length-1); j++){
+    //         if(a[j]>a[j+1]) // abcd, 1234
+    //             sawp(&a[j], &a[j+1]);
+    //     }
+    // }
+
+    bubble_sort(a,length);
+
     printf("After ...\n");
     print_array(a,length);
 
@@ -30,6 +34,15 @@ void sawp(int *a,int *b){
     int tmp = *a;
     *a = *b;
     *b = tmp;
+}
+
+void bubble_sort(int a[], int length){
+    for(int i=0; i<length; i++){
+        for(int j=0; j<(length-1); j++){
+            if(a[j]>a[j+1]) // abcd, 1234
+                sawp(&a[j], &a[j+1]);
+        }
+    }
 }
 
 void print_array(int array[], int length){
