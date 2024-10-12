5f893502f647e2fcf791107cfd31c5c850fa8996
beva3 <mizabienvenu@gmail.com>
2024-10-12 23:12:35 +0530

    str_chr

---
+++ 41WorldAcount.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int world_acount(char *s);
+
+int main(){
+    char *str_chr = strchr("Raphael",'R');
+    printf("The result is %s\n",str_chr);
+    return 0;
+}
+
+int world_acount(char *s){
+    int count = 0;
+
+    return count;
+}
