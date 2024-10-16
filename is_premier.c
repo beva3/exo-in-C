e117c1c0c9495d86ae31b26b0e792c0140bbbf0f
beva3 <mizabienvenu@gmail.com>
2024-10-16 05:43:04 +0530

    the prim number : start to end

--- is_premier.c
+++ is_premier.c
@@ -3,11 +3,13 @@
 
 bool is_prim(int n);
 void print_isPrim(int n);
-
+void prim(int sart, int end);
 int main(){
     int n = 67;
 
     print_isPrim(n);
+
+    prim(2,2000);
 
     return 0;
 }
@@ -26,7 +28,7 @@ bool is_prim(int n){
      *      ie : if m, n%m != 0,
      *                 n%m == 0 <=> m = only (1,n)
      */
-    printf("Is premier : ...\n");
+    // printf("Is premier : ...\n");
     // while(i<=n && n%i != 0) {
     while(i*i<=n && n%i != 0) {
         /**
@@ -34,8 +36,19 @@ bool is_prim(int n){
          * BUT, IF N IS VERY LARGE, THE LOOP MUST ...
          * SO : IF I<= RACINE2(I)
          */
-        printf("%d | %d [%d]\n",n,i,n%i);
+        // printf("%d | %d [%d]\n",n,i,n%i);
         i++;
     }
     return (i*i > n && n > 1) ? true : false;
+}
+
+void prim(int sart, int end){
+    int j=1;
+    for (int i = sart; i <= end; i++)
+    {
+        printf("%5d%c",is_prim(i)?i:0,(j%10 == 0)?'\n':' ');
+        j++;
+    }
+    printf("\n");
+    
 }
