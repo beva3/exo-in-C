6150371e17d6e7b0140fbd15752e2a6c9fc47460
beva3 <mizabienvenu@gmail.com>
2024-10-17 09:51:26 +0530

    fond sum

--- somme2_3.c
+++ somme2_3.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
+int s_n1(int n);
 int s_n2(int n);
+int s_n3(int n);
 int form_S_n2(int n);
 void print_s(int n);
 
@@ -8,6 +10,15 @@ int main(){
     print_s(9);
     return 0;
 }
+
+int s_n1(int n){
+    int s = 0;
+    for(int i = 1; i<=n; i++){
+        s += i;
+    }
+    return s;
+}
+
 int s_n2(int n){
     int s = 0;
     for(int i = 1; i<=n; i++){
@@ -15,13 +26,20 @@ int s_n2(int n){
     }
     return s;
 }
-
+int s_n3(int n){
+    int s = 0;
+    for(int i = 1; i<=n; i++){
+        s += i*i*i;
+    }
+    return s;
+}
 void print_s(int n){
     for(int i = 1; i<=n; i++)
         printf("%d%c",i,(i==n)?'\n':' ');
 
-    printf("s = %d : with loop for\n",s_n2(n));
-    printf("s = %d : with math formul\n",form_S_n2(n));
+    printf("s = %d : somme n1\n",s_n1(n));
+    printf("s = %d : somme n2\n",s_n2(n));
+    printf("s = %d : somme n3\n",s_n3(n));
 }
 
 int form_S_n2(int n){
