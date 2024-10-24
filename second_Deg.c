b8215ddb2bc00efceb34a9ce7c900b46e63ba914
beva3 <mizabienvenu@gmail.com>
2024-10-24 09:54:08 +0530

    solving equ second deg

---
+++ second_Deg.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+void solve_second_deg();
+
+int main(){
+    solve_second_deg();
+    return 0;
+}
+
+void solve_second_deg(){
+    printf("Solving second deg ....\n");
+}
