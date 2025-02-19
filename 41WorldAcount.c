9e706225500a1ec28a43b635f6b81bf0297e6a43
beva3 <mizabienvenu@gmail.com>
2025-02-19 18:30:12 +0530

    count word

--- 41WorldAcount.c
+++ 41WorldAcount.c
@@ -1,35 +1,73 @@
 #include <stdio.h>
 #include <string.h>
 
-
 /**
  * char *str_chr = strchr("Raphael",'p');
  *      Output : "phael"
- *      
+ *
  *      if the caracter is fond:
  *          teturn : ...
- *      if not found : it 
- *          return NULL    
+ *      if not found : it
+ *          return NULL
  */
-int world_acount(char *s);
+int word_count(char *s);
+int word_count_new(char *string);
 
-int main(){
-    printf("count : %d\n",world_acount("hello world how are you"));
+int main()
+{
+    char *my_string = "hello world, how are you.    And what's up";
+    printf("count : %d\n", word_count(my_string));
+    printf("count : %d\n", word_count_new(my_string));
     return 0;
 }
 
-int world_acount(char *s){
+int word_count(char *s)
+{
     int count = 1;
     while ((*s))
     {
         if ((*s) == ' ')
         {
-            count ++;
+            count++;
         }
-        *s++;
+        *s++; // from 0 to the end of string
+        // printf("s = %p\n",s);
+    }
+
+    return count;
+}
+int word_count_new(char *string)
+{
+    int len = strlen(string);
+    char *nonWord = " ,;.";
+    int count = 0;
+
+    for (int i = 0; i < len; i++)
+    {
+        while (i < len)
+        {
+            if (strchr(nonWord, string[i]))
+            {
+                // printf("%c strchr(nonWord, string[i]) NOT NULL\n", string[i]);
+                break;
+            }
+            i++;
+        }
+        count ++;
+
+        while (i < len)
+        {
+            if (!strchr(nonWord, string[i]))
+            {
+                // printf("%c strchr(nonWord, string[i])  NULL\n", string[i]);
+                break;
+            }
+            i++;
+        }
         
+
         
     }
-    
+
     return count;
 }
