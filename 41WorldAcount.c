5a9e0f7880148682bf7f32e7c7e49bc8788a8b57
beva3 <mizabienvenu@gmail.com>
2025-02-19 19:07:50 +0530

    ok

--- 41WorldAcount.c
+++ 41WorldAcount.c
@@ -1,71 +1,46 @@
 #include <stdio.h>
 #include <string.h>
 
-/**
- * char *str_chr = strchr("Raphael",'p');
- *      Output : "phael"
- *
- *      if the caracter is fond:
- *          teturn : ...
- *      if not found : it
- *          return NULL
- */
-int word_count(char *s);
-int word_count_new(char *string);
+int count_word(char *word);
 
 int main()
 {
-    char *my_string = "hello world, how are you.    And what's up";
-    printf("count : %d\n", word_count(my_string));
-    printf("count : %d\n", word_count_new(my_string));
+    char *word = "hello     you je suis";
+    int count = count_word(word);
+    printf("count world %d\n", count);
+    // printf("%s\n", strchr("qwerty",'e'));
+
     return 0;
 }
 
-int word_count(char *s)
+int count_word(char *word)
 {
-    int count = 1;
-    while ((*s))
-    {
-        if ((*s) == ' ')
-        {
-            count++;
-        }
-        *s++; // from 0 to the end of string
-        // printf("s = %p\n",s);
-    }
-
-    return count;
-}
-int word_count_new(char *string)
-{
-    int len = strlen(string);
-    char *nonWord = " ,;.";
+    int len_word = strlen(word);
+    char *non_word = " .,";
     int count = 0;
 
-    for (int i = 0; i < len; i++)
+    for (int i = 0; i < len_word; i++)
     {
-        while (i < len)
+        while (i < len_word)
         {
-            if (strchr(nonWord, string[i]))
+            if (strchr(non_word, word[i]))
             {
-                // printf("%c strchr(nonWord, string[i]) NOT NULL\n", string[i]);
                 break;
             }
             i++;
         }
-        count ++;
-
-        while (i < len)
+        printf("index = %d\n", i);
+        count++;
+        while (i<len_word)
         {
-            if (!strchr(nonWord, string[i]))
+            if (!strchr(non_word,word[i]))
             {
-                // printf("%c strchr(nonWord, string[i])  NULL\n", string[i]);
-                break;
+               break;
             }
             i++;
+            
         }
         
-
         
     }
 
